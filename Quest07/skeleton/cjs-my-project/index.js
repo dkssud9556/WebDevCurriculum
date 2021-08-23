793e742183cd6a18ff4ea73ad65e79946867f446
Kim Daewoong <daewoong.kim@knowre.com>
2021-08-23 17:25:59 +0900

    Quest07 skeleton

--- Quest07/skeleton/cjs-my-project/index.js
+++ Quest07/skeleton/cjs-my-project/index.js
@@ -1,1 +1,16 @@
-// TODO: cjs-package와 esm-package의 함수와 클래스들을 가져다 쓰고 활용하려면 어떻게 해야 할까요?
+const {cjsUtilFunction, CjsUtilClass} = require('../cjs-package/index');
+async function useEsmPackage () {
+  const {esmUtilFunction, EsmUtilClass} = await import('../esm-package/index.mjs');
+
+  const esmUtilClass = new EsmUtilClass(3);
+  console.log(esmUtilClass.double());
+
+  console.log(esmUtilFunction('hello!'));
+}
+
+useEsmPackage();
+
+const cjsUtilClass = new CjsUtilClass(3);
+console.log(cjsUtilClass.double());
+
+console.log(cjsUtilFunction('hello!'));
--- Quest07/skeleton/esm-my-project/index.mjs
+++ Quest07/skeleton/esm-my-project/index.mjs
@@ -1,1 +1,12 @@
-// TODO: cjs-package와 esm-package의 함수와 클래스들을 가져다 쓰고 활용하려면 어떻게 해야 할까요?
+import {EsmUtilClass, esmUtilFunction} from '../esm-package/index.mjs';
+import {CjsUtilClass, cjsUtilFunction} from '../cjs-package/index.js';
+
+const esmUtilClass = new EsmUtilClass(3);
+console.log(esmUtilClass.double());
+
+console.log(esmUtilFunction('hello!'));
+
+const cjsUtilClass = new CjsUtilClass(3);
+console.log(cjsUtilClass.double());
+
+console.log(cjsUtilFunction('hello!'));
