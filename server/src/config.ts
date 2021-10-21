473374e4f1b7220fdf4706a55a36ddbcbfd717ac
Kim Daewoong <daewoong.kim@knowre.com>
2021-10-21 22:49:32 +0900

    Quest19 수정

--- server/src/config.ts
+++ server/src/config.ts
@@ -10,7 +10,10 @@ export default process.env.NODE_ENV === 'test' ? {
     SEQUELIZE_OPTION: {
         dialect: 'mysql',
         username: process.env.MYSQL_USERNAME,
-        password: process.env.MYSQL_PASSWORD
+        password: process.env.MYSQL_PASSWORD,
+        database: process.env.MYSQL_DATABASE,
+        host: process.env.MYSQL_HOST,
+        port: process.env.MYSQL_PORT
     },
     ELASTIC_SEARCH_URL: process.env.ELASTIC_SEARCH_URL
 }
